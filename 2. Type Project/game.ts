ae5c7196ff805f87f5a4ccdae038c470c46aaed7
unknown <zmzmvkvk@naver.com>
2025-02-28 14:24:56 +0900

    type project 나머지도 완

--- 2. Type Project/game.ts
+++ 2. Type Project/game.ts
@@ -1,12 +1,38 @@
 /**
  * Let's make a game 🕹
  */
+type Direction = "up" | "down" | "left" | "right";
+let position = { x: 0, y: 0 };
+const move = (direction: Direction): void => {
+  switch (direction) {
+    case "up":
+      // position.x++;
+      position.y++;
+      break;
+    case "down":
+      // position.x++;
+      position.y--;
+      break;
+    case "left":
+      position.x--;
+      // position.y++;
+      break;
+    case "right":
+      position.x++;
+      // position.y++;
+      break;
+
+    default:
+      break;
+  }
+};
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right");
 console.log(position); // { x: 0, y: 0}
--- 2. Type Project/loading.ts
+++ 2. Type Project/loading.ts
@@ -3,24 +3,33 @@
    * Print Loading State
    */
   type LoadingState = {
-    state: 'loading';
+    state: "loading";
   };
 
   type SuccessState = {
-    state: 'success';
+    state: "success";
     response: {
       body: string;
     };
   };
 
   type FailState = {
-    state: 'fail';
+    state: "fail";
     reason: string;
   };
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+  const printLoginState = (login: ResourceLoadState): void => {
+    if (login.state == "success") {
+      console.log("😃 loaded");
+    } else if (login.state == "loading") {
+      console.log("👀 loading...");
+    } else {
+      console.log("😱 no network");
+    }
+  };
+  printLoginState({ state: "loading" }); // 👀 loading...
+  printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
 }
