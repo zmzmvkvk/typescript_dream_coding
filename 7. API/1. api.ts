58decc6f9a779b0d480739cc7d5cf372e432a4f5
unknown <zmzmvkvk@naver.com>
2025-03-07 13:37:04 +0900

    api

--- 7. API/1. api.ts
+++ 7. API/1. api.ts
@@ -1,2 +1,31 @@
-Array;
-[1, 2].map;
+{
+  Array;
+  [1, 2].map;
+
+  type Student = {
+    passed: boolean;
+  };
+
+  const students: Student[] = [{ passed: true }, { passed: true }, { passed: false }];
+  const result = students.every((student) => {
+    return student.passed;
+  }); // 모든 학생들이 true면 true. 하나라도 false면 false로 리턴됨
+
+  // console.log(result);
+
+  class Animal {}
+  class Cat extends Animal {
+    isCat: boolean = true;
+  }
+  class Dog extends Animal {
+    isDog: boolean = false;
+  }
+
+  const animals: Animal[] = [new Cat(), new Cat(), new Cat()];
+
+  function isCat(animal: Animal): animal is Cat {
+    return (animal as Cat).isCat !== undefined;
+  }
+
+  console.log(animals.every<Cat>(isCat));
+}
