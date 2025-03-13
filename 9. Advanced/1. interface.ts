5f918c9133e7590b053a92060b8e9b9a9ccca3e3
unknown <zmzmvkvk@naver.com>
2025-03-13 10:32:21 +0900

    interface type 차이

--- 9. Advanced/1. interface.ts
+++ 9. Advanced/1. interface.ts
@@ -9,6 +9,10 @@
     y: number;
   }
 
+  interface PositionInterface {
+    z: number;
+  }
+
   const obj1: PositionType = {
     x: 1,
     y: 1,
@@ -17,6 +21,7 @@
   const obj3: PositionInterface = {
     x: 1,
     y: 1,
+    z: 1,
   };
 
   class Pos1 implements PositionType {
@@ -27,5 +32,21 @@
   class Pos2 implements PositionInterface {
     x: number;
     y: number;
+    z: number;
   }
+  interface ZpositionInterface extends PositionInterface {
+    z: number;
+  }
+
+  type ZpositionType = PositionType & { z: number };
+
+  type Person = {
+    name: string;
+    age: number;
+  };
+
+  type Name = Person["name"]; // string
+
+  type NumberType = number;
+  type Direction = "left" | "right";
 }
